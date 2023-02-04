58460109e8ec81275423b21faf015c034608d4b5
Gamze Karasu <116583283+karasugamze@users.noreply.github.com>
2023-02-04 10:14:10 +0300

    Update wordle.py

--- wordle.py
+++ wordle.py
@@ -1,9 +1,9 @@
 import sys
 
-WoD=sys.argv[0]
-
+WoD = sys.argv[1]
 counter=0
-while counter<5:
+
+while counter<6:
     Try = input("Try:" )
     while len(Try)>5:
         print("The length of word must be five")
@@ -12,12 +12,9 @@ while counter<5:
     while len(Try) < 5:
         print("The length of word must be five")
         counter += 1
-        Try = input("Try:")
+        Try = input("Try")
     while len(Try)==5:
-        if Try==WoD:
-            print("Congratulations! You guess right !")
-            break
-        else:
+        if Try!=WoD:
             for i in range(0,4):
                 if Try[i] == WoD[i]:
                     print(i + 1, ". letter exist and located in right position.")
@@ -27,5 +24,9 @@ while counter<5:
                     print(i + 1, ".letter exist but located in wrong position.")
                 else:
                     print(i + 1, ". letter does not exist.")
-        counter+=1
+            counter+=1
+            break
+        else:
+            print("Congratulations! You guess right !")
         break
+    break
